117f21f8fea13bdf0b527a94837146ef2f0b4725
nitinkaushik01 <nittin.kaushik@hotmail.com>
2020-01-19 12:24:02 -0600

    Initial Commit

--- Classification_Algorithms/Logistic_Regression/Logistics_Regression_Model.py
+++ Classification_Algorithms/Logistic_Regression/Logistics_Regression_Model.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[94]:
+# In[1]:
 
 
 #Import necessary libraries
@@ -13,56 +13,56 @@ import pandas_profiling
 get_ipython().run_line_magic('matplotlib', 'inline')
 
 
-# In[95]:
+# In[2]:
 
 
 #Perform Exploratory Data Analysis in just one line of code
 pandas_profiling.ProfileReport(pd.read_csv('Titanic_Dataset.csv'))
 
 
-# In[96]:
+# In[3]:
 
 
 #Import Titanic Dataset
 titanic_data = pd.read_csv('Titanic_Dataset.csv')
 
 
-# In[97]:
+# In[4]:
 
 
 #Count number of passenger who survived and NOT survived: 0 = Not Survived, 1 = Survived
 sns.countplot(x='Survived',data=titanic_data)
 
 
-# In[98]:
+# In[5]:
 
 
 #Count number of males and females who survived and NOT survived: 0 = Not Survived, 1 = Survived
 sns.countplot(x='Survived',hue='Sex',data=titanic_data)
 
 
-# In[99]:
+# In[6]:
 
 
 #Count which class passengers survived most
 sns.barplot(x='Pclass', y='Survived', data=titanic_data)
 
 
-# In[100]:
+# In[7]:
 
 
 #Verify missing values in each column
 titanic_data.isnull().sum()
 
 
-# In[101]:
+# In[8]:
 
 
 #Check data types of each column and hence finding out which columns are categorical in nature
 titanic_data.info()
 
 
-# In[102]:
+# In[9]:
 
 
 #Visualize and find out average age by class
@@ -70,7 +70,7 @@ plt.figure(figsize=(12, 8))
 sns.boxplot(x='Pclass',y='Age',data=titanic_data,palette='winter')
 
 
-# In[103]:
+# In[10]:
 
 
 #Function to impute missing values in age column based on class
@@ -83,21 +83,21 @@ def imput_age(cols):
         return Age
 
 
-# In[104]:
+# In[11]:
 
 
 #Impute missing values in age column by average age
 titanic_data["Age"] = titanic_data[["Age", "Pclass"]].apply(imput_age,axis=1)
 
 
-# In[105]:
+# In[12]:
 
 
 #Drop Cabin column
 titanic_data.drop("Cabin",inplace=True,axis=1)
 
 
-# In[106]:
+# In[13]:
 
 
 #Impute Embarked missing values with most common values
@@ -107,14 +107,14 @@ for data in titanic_data:
     titanic_data['Embarked'] = titanic_data['Embarked'].fillna(most_common_value)
 
 
-# In[107]:
+# In[14]:
 
 
 #Check if there are any missing values left
 titanic_data.isnull().sum()
 
 
-# In[108]:
+# In[15]:
 
 
 #Convert categorical variables into Dummy variables
@@ -123,21 +123,21 @@ embark = pd.get_dummies(titanic_data['Embarked'],drop_first=True)
 pclass = pd.get_dummies(titanic_data["Pclass"],drop_first=True)
 
 
-# In[109]:
+# In[16]:
 
 
 #Drop irrelevant columns
 titanic_data.drop(['PassengerId','Sex','Embarked','Name','Ticket','Pclass'],axis=1,inplace=True)
 
 
-# In[110]:
+# In[17]:
 
 
 #Validate the columns and data
 titanic_data.head()
 
 
-# In[111]:
+# In[18]:
 
 
 #Concatenate the dummy variables, created above
@@ -145,7 +145,7 @@ titanic_data = pd.concat([titanic_data,sex,embark,pclass],axis=1)
 titanic_data.head()
 
 
-# In[112]:
+# In[19]:
 
 
 #Create Feature variable X and Target variable y
@@ -153,7 +153,7 @@ X = titanic_data.drop("Survived",axis=1)
 y = titanic_data['Survived']
 
 
-# In[113]:
+# In[20]:
 
 
 #Split the data into training set (70%) and test set (30%)
@@ -161,7 +161,7 @@ from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=50)
 
 
-# In[114]:
+# In[21]:
 
 
 #Fit the logistic Regression Model
@@ -174,78 +174,49 @@ pred = logmodel.predict(X_test)
 pred
 
 
-# In[115]:
+# In[22]:
 
 
 #Check the values of first record correspoding to the first vector value given above.
 X_test.head(1)
 
 
-# In[ ]:
+# In[28]:
 
 
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[88]:
-
-
+#Calculate Precision, Recall and F1-Score
 from sklearn.metrics import classification_report
 print(classification_report(y_test,pred))
+
+
+# In[24]:
+
+
+#Generate confusion matrix
+from sklearn.metrics import confusion_matrix
+conf_mat = confusion_matrix(y_test,pred)
+conf_mat
+
+
+# In[26]:
+
+
+#Generate AUC-ROC curve
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_curve
+logit_roc_auc = roc_auc_score(y_test, logmodel.predict(X_test))
+fpr, tpr, thresholds = roc_curve(y_test, logmodel.predict_proba(X_test)[:,1])
+plt.figure()
+plt.plot(fpr, tpr, label='Logistic Regression (area = %0.2f)' % logit_roc_auc)
+plt.plot([0, 1], [0, 1],'r--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver Operating Characteristic')
+plt.legend(loc="lower right")
+plt.savefig('Log_ROC')
+plt.show()
 
 
 # In[ ]:
